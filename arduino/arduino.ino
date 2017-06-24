e09926a9ee8c802c0d5ac7b03fd4b87e3be12822
hkkenneth <hkkenneth@gmail.com>
2017-06-25 06:37:35 +0800

    Initial test

---
+++ arduino/arduino.ino
@@ -0,0 +1,24 @@
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Initialized");
+  
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  Serial.println("Go High");
+  delay(1000);                       // wait for a second
+
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  Serial.println("Go Low");
+  delay(1000);                       // wait for a second
+  
+}
